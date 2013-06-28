c8bc89753786e2fe48ab20c9384c7fa65225953e
Donna Khair <donna12@cs.huji.ac.il>
2013-06-28 10:23:05 +0300

    models added

--- storieshare/stories/models.py
+++ storieshare/stories/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+#class account (models.Model)
+#   username = models.CharField(max_length = 15)
+#    password = models.CharField(max_length = 15)
+#
+#    def _unicode_(self):
+#        return username
+
+class story (models.Model)
+    storyTitle = models.CharField(max_length = 25) 
+    storyLikes = models.IntegerField()
+    storyComments = models.CharField(max_length = 250)
+    
+class Paragraph (models.Model)
+    pargraphContent = models.CharField()
+    Story = models.ForeignKey("story")
+
+class Comments (models.Model)
+    Comment = models.ForeignKey("story")
+
+
+
+
